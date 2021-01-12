fe8d79b423b02245b00e7954ab51f0d66da3a56a
qlemql <busgamer@naver.com>
2021-01-12 15:54:33 +0900

    useTab
    Api나 다른 어떤 컴포턴트로부터 무언가를 가져오려할때 사용하기 좋다

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import React, { useState } from "react";
 import './App.css';
 
-const useInput = (initialValue, validator) =>{
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) =>{
-    const {
-      target : {value}
-    } = event;
-    let willUpdate = true;
-    if(typeof validator === "function"){
-      willUpdate = validator(value);
-    }
-    if(willUpdate){
-      setValue(value);
-    }
+
+const content = [
+  {
+    tap:"section 1",
+    content:"I'm the content of the section 1"
+  },
+  {
+    tap:"section 2",
+    content:"I'm the content of the section 2"
   }
-  return { value, onChange };
+]
+
+const useTabs = (initialTap, allTaps) =>{
+  const [currentIndex, setCurrentIndex]= useState(initialTap);
+  if(!allTaps || !Array.isArray(allTaps)){
+    return;
+  }
+  return{
+    currentItem:allTaps[currentIndex],
+    changeItem: setCurrentIndex
+  };
 }
 
 const App = () => {
-  const maxLen = (value) => value.length <= 10;
-  const name = useInput("Mr.", maxLen);
+  const {currentItem, changeItem} = useTabs(0, content);
   return (
     <div className="App">
       <h1> React Hook </h1>
-      <input placeholder="Name" value={name.value} onChange={name.onChange} />
-      {/* <input placeholder="Name" {...name} /> */}
+      {content.map((section, index) => <button onClick={()=>changeItem(index)}>{section.tap}</button>)}
+      <div>
+        {currentItem.content}
+      </div>
     </div>
   );
 }
