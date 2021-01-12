b39fcc0eb3827c54cc77d8ec4ed504808adc6c9e
qlemql <busgamer@naver.com>
2021-01-12 15:13:08 +0900

    useInput hooks
    useInput은 하나의 initialValue를 받는다
    
    const useInput = (initialValue) =>{
      const [value, setValue] = useState(initialValue);
      const onChange = (event) =>{
        console.log(event.target);
      }
      return { value, onChange };
    }
    
    const name = useInput("default");
    
    value={name.value} onChange={name.onChange} 대신에 {...name}으로 작성할수있다.
    name안에 있는 모든 것들을 풀어준다.(코드가 길어지는걸 줄여줌)
    
    +유효성을 검증하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,49 +1,34 @@
 import React, { useState } from "react";
 import './App.css';
 
-function App() {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+const useInput = (initialValue, validator) =>{
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) =>{
+    const {
+      target : {value}
+    } = event;
+    let willUpdate = true;
+    if(typeof validator === "function"){
+      willUpdate = validator(value);
+    }
+    if(willUpdate){
+      setValue(value);
+    }
+  }
+  return { value, onChange };
+}
+
+const App = () => {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
       <h1> React Hook </h1>
-      <h2> This is {item}</h2>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+      {/* <input placeholder="Name" {...name} /> */}
     </div>
   );
 }
 
-class AppTest extends React.Component{
-  state={
-    item : 1
-  }
-  render(){
-    const {item} = this.state;
-    return (
-      <div className="App">
-        <h1> React Hook </h1>
-        <h2> This is {item}</h2>
-        <button onClick={this.incrementItem}>Increment</button>
-        <button onClick={this.decrementItem}>Decrement</button>
-      </div>
-    );
-  }
-  incrementItem = () =>{
-    this.setState(state =>{
-      return{
-        item: state.item + 1
-      };
-    });
-  };
-  decrementItem = () =>{
-    this.setState(state =>{
-      return{
-        item: state.item - 1
-      };
-    });
-  };
-}
 
-export default AppTest;
+export default App;
